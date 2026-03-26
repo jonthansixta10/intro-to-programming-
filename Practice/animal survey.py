2eda225a52d890817b7ee22d3e1f162e85469c31
GIWolf2020! <jsixta@protonmail.com>
2026-03-26 15:19:57 -0500

    x

---
+++ Practice/animal survey.py
@@ -0,0 +1,9 @@
+fav_animal = input("what  is your favorite animal?\n->")
+where_live = input("where do they live?\n->")
+what_color = input("what color is it?\n->")
+fun_fact =print(" what is a fun fact about your favorite animal?\n->")
+
+print(" favorite animal " + fav_animal)
+print(" where does it live " + where_live)
+print(" what color is it " + what_color)
+print("what is a fun fact about it" + fun_fact)
