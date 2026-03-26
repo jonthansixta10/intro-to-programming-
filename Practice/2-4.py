5ebbce581b294826a0986ef54b6133e6786fd5b8
GIWolf2020! <jsixta@protonmail.com>
2026-03-26 15:20:03 -0500

    x

--- Practice/2-4.py
+++ Practice/2-4.py
@@ -1,1 +1,15 @@
 print(5 * 3)
+print(5//3)
+print(5**3)
+print(23//5)
+print(23 % 5)
+print(20//5)
+
+
+name = "Jon"
+age = 16
+
+print(name + " is " + str(age) + " years old ")
+
+time_left = 80-int(age)
+
