047237e5004dbec04e5eca69f8408ca753abbfc9
GIWolf2020! <jsixta@protonmail.com>
2026-04-06 15:07:49 -0500

    t

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,18 @@
+# In a galaxy far far away, on the [planet] [noun1],
+# there lived a [animal1] [noun2].
+# One time, a [amimal2][noun3] appeared
+# and [verb] the [noun4] away
+# then the [noun5] was [adjective] and [emotion]
+
+planet = input("name a planet\n->")
+noun1 = input("put in a noun\n->")
+animal1 = input("enter an animal\n->")
+noun2 = input("put in another noun\n->")
+animal2 = input("put in another animal\n->")
+noun3 = input("give another noun\n->")
+verb = input("give a verb\n->")
+noun4 = input("give a noun\n->")
+adjective = input("give an adjective\n->")
+emotion = input("give and emotion\n->")
+noun5 = input("give a noun\n->")
+print("In a galaxy far far away, on the" + planet, noun1, "there lived a" + animal1, noun2,". one time a" + animal2, noun3, "apeared and" + verb,"the" + noun4, "away then the" + noun5, "was" + adjective, "was" + emotion)
