f5064443a43299553fc186c9ac1bb0aece5e8586
GIWolf2020! <jsixta@protonmail.com>
2026-04-07 14:52:15 -0500

    def

---
+++ Practice/2-6.py
@@ -0,0 +1,15 @@
+def hello():
+    print("hello")
+    print("hello again")
+
+print("not indented")
+hello()
+
+
+
+def add(x = 0, y = 0): #this functions purpose is to add two numbers
+    print(x+y)
+
+add(2, 10)
+
+
