b343b5c048e6c2bb1c3bd06a517cf1152864f412
GIWolf2020! <jsixta@protonmail.com>
2026-04-09 14:42:08 -0500

    boolean Quiz

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,11 @@
+answer1 = int(input("give a whole number that is less than 7.\n->"))
+answer2 = int(input("give a whole number that is more than 12.\n->"))
+answer3 = int(input("give a whole number that is equal to 150.\n->"))
+answer4 = int(input("give a whole number that is great than or equal to 750.\n->"))
+answer5 = int(input("give a whole number that is less than or equal to 966.\n->"))
+
+print(answer1 < 7)
+print(answer2 > 12)
+print(answer3 == 150)
+print(answer4 >= 750)
+print(answer5 <= 966)
