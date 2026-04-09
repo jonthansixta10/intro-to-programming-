1b24d97863489fb1d70dbb6217757ce4ec0f96df
GIWolf2020! <jsixta@protonmail.com>
2026-04-09 14:26:26 -0500

    calculator

---
+++ Assignments/SimpleCalculator.py
@@ -0,0 +1,15 @@
+x = float(input("enter a number\n->"))
+y = float(input("enter a number\n->"))
+def addition(x,y):
+    return (x+y)
+def subtract(x,y):
+    return (x-y)
+def multiply(x,y):
+    return (x*y)
+def divide(x,y):
+    return (x/y)
+
+print(addition(x,y))
+print(subtract(x,y))
+print(multiply(x,y))
+print(divide(x,y))
---
+++ Practice/Calculate Tax.py
@@ -0,0 +1,6 @@
+
+item = input("name an item")
+price = input("say the price")
+tax = (price/6.875)
+total = (tax +price )
+print(item, " costs ", + price, " dollors before tax and ", + total, " after tax")
