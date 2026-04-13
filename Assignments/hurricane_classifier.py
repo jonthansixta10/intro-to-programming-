567b2b8a0180d1d319357d018881ff47865558d7
GIWolf2020! <jsixta@protonmail.com>
2026-04-13 14:44:59 -0500

    hurricane classifier

---
+++ Assignments/hurricane_classifier.py
@@ -0,0 +1,14 @@
+wind_speed = float(input("enter the wind speed\n->"))
+
+if wind_speed < 74:
+    print("Tropical storm")
+elif wind_speed < 96:
+    print("Category 1")
+elif wind_speed < 111:
+    print("Category 2")
+elif wind_speed < 130:
+    print("Category 3")
+elif wind_speed < 157:
+    print("Category 4")
+else:
+    print("Category 5")
