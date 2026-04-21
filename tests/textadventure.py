b19b6e1faa621a22fb39e2e3a11162d52c3a4203
GIWolf2020! <jsixta@protonmail.com>
2026-04-21 15:06:53 -0500

    the game

--- tests/textadventure.py
+++ tests/textadventure.py
@@ -33,9 +33,9 @@ def start_game(num_teammates):
 def randomize_loadout():
     """Generate a random loadout and return it"""
     loadouts = [
-        "ferro 2 with a kettle two and 3 shield rechargers and 5 bandages and 3 snap gernades",
-        "osprey with a sticher 2 and 2 shield rechargers and 3 bandages and 2 smoke gernades",
-        "anvil 2 and a torento with 5 shield rechargers and 5 bandages and 5 snap gernades"
+        "ferro, 1 shield rechargers and 2 bandages and 2 snap gernades",
+        "sticher, 1 shield rechargers and 2 bandages and 1 defibulator",
+        "kettle, 1 shield rechargers and 2 bandages and 2 jolt mines"
     ]
     return random.choice(loadouts)
 
@@ -61,14 +61,17 @@ def custom_loot():
     print("3. a anvil 2 and a torento with 5 shield rechargers and 5 bandages and 5 snap gernades")
     choice = input("> ")
     if choice == "1":
+        loadout = "ferro 2 with a kettle two and 3 shield rechargers and 5 bandages and 3 snap gernades"
         print("you have chosen the ferro 2 loadout")
-        choose_event()
+        choose_event(loadout)
     elif choice == "2":
+        loadout = "osprey with a sticher 2 and 2 shield rechargers and 3 bandages and 2 smoke gernades"
         print("you have chosen the osprey loadout")
-        choose_event()
+        choose_event(loadout)
     elif choice == "3":
+        loadout = "anvil 2 and a torento with 5 shield rechargers and 5 bandages and 5 snap gernades"
         print("you have chosen the anvil 2 and torento loadout")
-        choose_event()
+        choose_event(loadout)
     else:
         print("invalid choice, please try again")
         custom_loot()
@@ -102,16 +105,13 @@ def choose_map(event_type, loadout=None):
         choice = input("> ")
         if choice == "1":
             print("you have chosen Buried City")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            buried_city_hurricane(loadout)
         elif choice == "2":
             print("you have chosen blue gate")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            blue_gate_hurricane(loadout)
         elif choice == "3":
             print("you have chosen space port")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            space_port_hurricane(loadout)
         else:
             print("invalid choice, please try again")
             choose_map(event_type, loadout)
@@ -122,16 +122,13 @@ def choose_map(event_type, loadout=None):
         choice = input("> ")
         if choice == "1":
             print("you have chosen dam battlegrounds")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            dam_closed_security(loadout)
         elif choice == "2":
             print("you have chosen blue gate")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            blue_gate_closed_security(loadout)
         elif choice == "3":
             print("you have chosen Buried City")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            buried_city_closed_security(loadout)
         else:
             print("invalid choice, please try again")
             choose_map(event_type, loadout)
@@ -142,17 +139,66 @@ def choose_map(event_type, loadout=None):
         choice = input("> ")
         if choice == "1":
             print("you have chosen Stella Montis")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            stella_montis_night_raid(loadout)
         elif choice == "2":
             print("you have chosen blue gate")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            blue_gate_night_raid(loadout)
         elif choice == "3":
             print("you have chosen Buried City")
-            print(f"Your loadout: {loadout}")
-            print("good luck!")
+            buried_city_night_raid(loadout)
         else:
             print("invalid choice, please try again")
             choose_map(event_type, loadout)
+def buried_city_hurricane(loadout):
+    print("you have entered the buried city map for the hurricane event")
+    print("good luck!")
+    choose_loot_spot(loadout)
+
+    def choose_loot_spot(loadout):
+        print("where do you want to go looting?")
+        print("1. the new District, it is a high tier loot spot but that means that thier is a greater chance of running into other players")
+        print("2. the old town, it has a medium tier loot but that means that there is a lower chance of running into other players")
+        print("3. the west villige, it has meduim tier loot and wis on the outsides of the map so the chance of running into plays is very low")
+        choice = input("> ")
+        if choice == "1":
+            new_district(loadout)
+        elif choice == "2":
+            old_town(loadout)
+        elif choice == "3":
+            west_villige(loadout)
+
+    def new_district(loadout):
+        print("you have entered the new District")
+        print("you hear a raider flare go off in the distance meaning a player jsut got nocked")
+        print("you have a choice to make")
+        print("1. go to the flare and try to get the players loot")
+        print("2. ignore the flare and keep looting")
+        choice = input("> ")
+        if choice == "1":
+            print("you have chosen to go to the flare")
+            print("you run towards the flare and you see a player looting the body")
+            print("you have a choice to make")
+            print("1. try to sneak up on the player and kill them")
+            print("2. try to outplay the player and kill them")
+            print("3. run away and try to find another loot spot")
+            choice = input("> ")
+            if choice == "1":
+                print("you have chosen to sneak up on the player")
+                print("you successfully sneak up on the player and kill them, you get their loot and your invinotry is now full of very good items!")
+            elif choice == "2":
+                print("you have chosen to outplay the player")
+                print("you try to outplay the player but they are too good and they end up killing you, better luck next time!")
+            elif choice == "3":
+                print("you have chosen to run away")
+                print("you run away and you find another loot spot but it is not as good as the one you just left, better luck next time!")
+            else:
+                print("invalid choice, please try again")
+                new_district(loadout)
+        elif choice == "2":
+            print("you have chosen to ignore the flare")
+            print("you ignore the flare and you keep looting, you end up finding some good loot but you are not in a good position to win the game, better luck next time!")
+        else:
+            print("invalid choice, please try again")
+            new_district(loadout)
+    
 start_adventure()
