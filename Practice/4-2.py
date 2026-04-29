738cd43d2cbfe8bf656d3ac4c73d77a250816ec4
jonthansixta10 <jsixta@protonmail.com>
2026-04-29 14:15:27 -0500

    the

---
+++ Practice/4-2.py
@@ -0,0 +1,20 @@
+count_down = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+for number in count_down:
+    print(number)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+for number in numbers:
+    total += number
+print(total)
+
+
+list = [1, 2, 3, 4, 5]
+
+
+numbers1 = [1, 2, 3, 4, 5]
+squares = []
+for n in numbers1:
+    squares.append(n ** 2)
+print(squares)
