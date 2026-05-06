4923e80555011308eac8de730164e1b82efcca34
jonthansixta10 <jsixta@protonmail.com>
2026-05-06 14:39:25 -0500

    the

--- Practice/4-5.py
+++ Practice/4-5.py
@@ -1,9 +1,14 @@
-my_dict = {"Alice": "A",
-            "Bob": "B",
-            "Charlie": "C",
-            "David": "A",
-            "Eve": "B"
-           }
-print(my_dict)
-for name, grade in my_dict.items():
-    print(f"{name}: {grade}")
+student = {"name": "Alice", "age": 16, "grade": "A"}
+student["grade"] = "A+"
+print(student["name"], student["age"], student["grade"])
+
+
+favorite_movies = [{"title": "Inception", "year": 2010}, {"title": "The Matrix", "year": 1999}, {"title": "gladiator", "year": 2000}]
+new_movie = input("Enter the title of the new movie: ")
+date = int(input("Enter the release year of the new movie: "))
+favorite_movies.append({"title": new_movie, "year": date})
+print("Updated list of favorite movies:")
+for movie in favorite_movies:
+    print(f"{movie['title']} ({movie['year']})")
+
+
